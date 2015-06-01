4a9057890aebb2ea2ccc4e62f6cc42ca504de9c3
Keith Conroy <kconroyjr@gmail.com>
2015-05-31 20:45:21 -0700

    remove js geolocation

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -4,30 +4,4 @@ $(document).on('page:change', function() {
   },function(){
       $(this).removeClass('flip');
   });
-  getLocation();
-
 });
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-  var data = { lat: lat, lon: lon};
-  $.ajax({
-    url: 'http://localhost:3000/hobbies/coordinates',
-    type: 'get',
-    data: data
-  }).done(function(data) {
-    context = {categories: data}
-    $('#categories').html(template(context))
-  }).fail(function() {
-      console.log('error');
-  });
-}
