8ec4dc8c8a39d2c3f0b898c158ffd09d7e3ead99
Caitlyn Yu <caitlynmyu@gmail.com>
2015-06-01 21:03:01 -0700

    add edit link to profile, redirect to edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit]
   def show
+  end
+
+  def edit
+  end
+
+  private
+  def find_user
     @user = User.find(params[:id])
   end
 end
