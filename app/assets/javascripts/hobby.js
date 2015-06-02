c104574ff1c8e9e23d85b1da779cbc3845ec2ba3
Keith Conroy <kconroyjr@gmail.com>
2015-06-02 13:58:23 -0700

    overwrite foundations modal show

--- app/assets/javascripts/hobby.js
+++ app/assets/javascripts/hobby.js
@@ -3,32 +3,10 @@ $(document).on('page:change', function() {
 });
 
 function bindHobbyEvents() {
-  // $('.new_hobby').on('submit', createHobby);
+  $('#add_hobby_button').on('click', showHobbyModal);
 };
 
-function createHobby() {
-  event.preventDefault();
-  // debugger
-  var data = $(this).serialize();
-  var img = $(this).find("input[type='file']").val()
-  // debugger
-  var response = $.ajax({
-    url: '/hobbies',
-    type: 'post',
-    data: data
-  }).done(function(hobby) {
-    debugger
-    var modal = $('#new-hobby-modal');
-
-    modal.foundation('reveal', 'close');
-    modal.find('textarea').val('');
-    $("input[id='hobby_name']").val('');
-    $("input[id='hobby_title']").val('');
-
-    $('#show_category').append(hobby);
-
-    bindQuestionEvents();
-  }).fail(function() {
-    console.log('error');
-  });
+function showHobbyModal(){
+  $('#new-hobby-modal').foundation('reveal', 'open');
 }
+
