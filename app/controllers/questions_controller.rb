ef66679af265ba480a7aafea8a7477eff26506e7
keithconroy <kconroyjr@gmail.com>
2015-06-05 15:20:29 -0700

    add controller actions back in

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,10 +3,47 @@ class QuestionsController < ApplicationController
   before_action :questions_by_hobby, only: [:search, :sort]
   before_action :new_answer, only: [:create, :search, :sort]
 
+  def index
+    questions = Question.order('created_at DESC').all
+  end
+
   def create
     @question = Question.new(question_params)
     @question.save
     return render partial: 'question', layout: false, locals: { question: @question, :@answer => @answer }
+  end
+
+  def new
+    question = Question.new
+    render json: question
+  end
+
+  def edit
+    if compare_user?
+      render json: @question
+    else
+      render json: {errors: 'Unauthorized request'}, status: 401
+    end
+  end
+
+  def show
+    answers = @question.answers.order('created_at DESC').all
+  end
+
+  def update
+    @question.update_attributes(question_params)
+  end
+
+  def destroy
+    if @question
+      if compare_user?
+        @question.destroy
+      else
+        render json: {errors: 'Unauthorized request'}, status: 401
+      end
+    else
+      render json: {errors: 'Bad Request'}, status: 400
+    end
   end
 
   def vote
