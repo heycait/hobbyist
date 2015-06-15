2c6f25a4e00efd2b8cbfdabad7f0207a5c3b22ad
Caitlyn Yu <caitlynmyu@gmail.com>
2015-06-15 16:05:41 -0700

    add about route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
+
+  get 'about/show'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", registrations: 'registrations' }
   devise_scope :user do
     authenticated :user do
@@ -26,7 +30,7 @@ Rails.application.routes.draw do
   resources :answers
   get '/answers/:id/:vote' => 'answers#vote'
 
-
+  get '/about' => 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
